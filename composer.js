01b617e6f3cddd36b10ab46a237ceae166184f3b
Andrew Danger Lyon <orthecreedence@gmail.com>
2012-07-19 20:33:50 -0700

    fixing bug if process_querystring logic. was ignoring route changes in querystring unless force was set. not really in the spirit of what i wanted, so now any route change will trigger the route, querystring processing or not.

--- composer.js
+++ composer.js
@@ -1705,14 +1705,10 @@
 		{
 			if(path && path.stop != undefined) path = false;
 			path || (path = this.cur_path());
-
-			// remove querystring from the url if we have set the Router to
-			// ignore it
-			if(!this.options.process_querystring) path = path.replace(/\?.*/, '');
-
 			force	=	!!force;
 
-			// remove the motherfucking ! at the beginning
+			// check if we are routing to the same exact page. if we are, return
+			// (unless we force the route)
 			if(this.last_path == path && !force)
 			{
 				// no need to reload
@@ -1720,6 +1716,12 @@
 			}
 
 			this.last_path	=	path;
+
+			// remove querystring from the url if we have set the Router to
+			// ignore it. Note that this happens after the same-page check since
+			// we still want to take QS into account when comparing URLs.
+			if(!this.options.process_querystring) path = path.replace(/\?.*/, '');
+
 			this.trigger('preroute', path);
 			this.trigger('route', path);
 		},
