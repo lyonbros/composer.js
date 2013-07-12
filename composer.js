0666c3e29a9520770b65217e919ef35771553cb2
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-07-11 19:00:24 -0700

    adding Collection.filter. not sure how this ever slipped through. `search` is great, but not really as standard as filter.

--- composer.js
+++ composer.js
@@ -1146,6 +1146,15 @@
 		},
 
 		/**
+		 * Filter this collection's models by the given callback. Works just
+		 * like Array.filter in JS.
+		 */
+		filter: function(callback)
+		{
+			return this._models.filter(callback);
+		},
+
+		/**
 		 * query the models in the collection with a callback and return ALL that
 		 * match. takes either a function OR a key-value object for matching:
 		 *
