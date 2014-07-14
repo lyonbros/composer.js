452c118b9689b3471c95ac51860161b3b0170599
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-07-14 11:47:06 -0700

    removing keyboard module from composer. its a bit tangential to the project, and easily implementable WITHOUT composer as a standalone module.

--- test/spec.keyboard.js
+++
@@ -1,6 +0,0 @@
-describe('Composer.Keyboard', function() {
-	it('will not be tested at this particular juncture because triggering keyboard events programatically is a huge pain in the ass', function() {
-		expect(true).toBe(true);
-	});
-});
-
