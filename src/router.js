29039168528c60e325df40f1c7af8fcaaafa8099
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-07-25 20:02:27 -0500

    adding (and testing) router.options.base for supporting base URLs when routing.

--- src/router.js
+++ src/router.js
@@ -45,7 +45,8 @@
 		options: {
 			suppress_initial_route: false,
 			enable_cb: function(url) { return true; },
-			process_querystring: false
+			process_querystring: false,
+			base: false
 		},
 
 		/**
@@ -98,6 +99,15 @@
 			this.unbind();
 		},
 
+		debasify: function(path)
+		{
+			if(this.options.base && path.indexOf(this.options.base) == 0)
+			{
+				path = path.substr(this.options.base.length);
+			}
+			return path;
+		},
+
 		/**
 		 * get the current url path
 		 */
@@ -111,7 +121,7 @@
 			{
 				var path = global.location.pathname+global.location.search;
 			}
-			return path;
+			return this.debasify(path);
 		},
 
 		/**
@@ -139,8 +149,9 @@
 			options || (options = {});
 			options.state || (options.state = {});
 
-			var href = '/' + url.trim().replace(/^[a-z]+:\/\/.*?\//, '').replace(/^[#!\/]+/, '');
-			var old = this.cur_path();
+			var base = (this.options.base || '');
+			var href = base + '/' + url.trim().replace(/^[a-z]+:\/\/.*?\//, '').replace(/^[#!\/]+/, '');
+			var old = base + this.cur_path();
 			if(old == href)
 			{
 				this.trigger('statechange', href, true);
@@ -245,7 +256,8 @@
 		state_change: function(path, force)
 		{
 			if(path && path.stop != undefined) path = false;
-			path || (path = this.cur_path());
+			if(path) path = this.debasify(path);
+			if(!path) path = this.cur_path();
 			force = !!force;
 
 			// check if we are routing to the same exact page. if we are, return
--- test/spec.router.js
+++ test/spec.router.js
@@ -19,6 +19,7 @@ describe('Composer.Router', function() {
 
 		handlers.users = function() { loaded_users++; };
 		router = new Composer.Router(routes);
+		router.unbind('fail');
 		router.bind('fail', function() { fails++; });
 
 		expect(router instanceof Composer.Router).toBe(true);
@@ -37,6 +38,7 @@ describe('Composer.Router', function() {
 			load_user: function(uid) { loaded_user = uid; }
 		};
 
+		router.unbind('fail');
 		router.bind('fail', function(err) { error = err });
 		router.route('/');
 		expect(loaded_home).toBe(1);
@@ -48,9 +50,52 @@ describe('Composer.Router', function() {
 		expect(error).toBe(false);
 	});
 
+	it('will handle rebased urls properly', function() {
+		router.options.base = '/slappy';
+
+		var loaded_home = 0;
+		var loaded_users = 0;
+		var loaded_user = false;
+		var error = false;
+		handlers = {
+			home: function() { loaded_home++; },
+			users: function() { loaded_users++; },
+			load_user: function(uid) { loaded_user = uid; }
+		};
+
+		var get_path = function()
+		{
+			if(History.emulated.pushState)
+			{
+				return window.location.hash.replace(/^[#!]+/, '');
+			}
+			else
+			{
+				return window.location.pathname;
+			}
+		};
+
+		router.unbind('fail');
+		router.bind('fail', function(err) { error = err });
+		router.route('/');
+		expect(get_path()).toBe('/slappy/');
+		expect(loaded_home).toBe(1);
+		router.route('/users');
+		expect(get_path()).toBe('/slappy/users');
+		expect(loaded_users).toBe(1);
+		router.route('/users/27');
+		expect(get_path()).toBe('/slappy/users/27');
+		expect(loaded_user).toBe('27');
+
+		expect(error).toBe(false);
+
+		router.options.base = false;
+	});
+
 	// bring us back to the original route!
 	it('will fail on unknown route', function() {
 		var error = false;
+		router.unbind('fail');
 		router.bind('fail', function(err) { error = err; });
 		router.route(initial);
 
