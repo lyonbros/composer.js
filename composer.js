c95923f0317106d9faedb33f495e0cccde4b9571
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-09-16 08:46:00 -0700

    trimming down Adapter.fire_event (still works in ie6+)

--- composer.js
+++ composer.js
@@ -2111,68 +2111,16 @@
 		 * @param {HTMLNode} node The node to fire the event handler on.
 		 * @param {String} eventName The name of the event without the "on" (e.g., "focus")
 		 */
-		return function(node, eventName, options) {
+		return function(el, type, options) {
 			options || (options = {});
 
-			if(eventName == 'click' && node.click)
+			if(type == 'click' && el.click)
 			{
-				return node.click();
+				return el.click();
 			}
 
-			// Make sure we use the ownerDocument from the provided node to avoid cross-window problems
-			var doc;
-			if (node.ownerDocument) {
-				doc = node.ownerDocument;
-			} else if (node.nodeType == 9){
-				// the node may be the document itself, nodeType 9 = DOCUMENT_NODE
-				doc = node;
-			} else {
-				throw new Error("Invalid node passed to fireEvent: " + node.id);
-			}
-
-			if (node.dispatchEvent) {
-				// Gecko-style approach (now the standard) takes more work
-				var eventClass = "";
-
-				// Different events have different event classes.
-				// If this switch statement can't map an eventName to an eventClass,
-				// the event firing is going to fail.
-				switch (eventName) {
-				case "click": // Dispatching of 'click' appears to not work correctly in Safari. Use 'mousedown' or 'mouseup' instead.
-				case "mousedown":
-				case "mouseup":
-					eventClass = "MouseEvents";
-					break;
-
-				case "focus":
-				case "change":
-				case "blur":
-				case "select":
-					eventClass = "HTMLEvents";
-					break;
-
-				case "keyup":
-				case "keydown":
-					eventClass = "KeyboardEvent";
-					break;
-
-				default:
-					throw "fireEvent: Couldn't find an event class for event '" + eventName + "'.";
-					break;
-				}
-
-				var bubbles = true;
-				var event = doc.createEvent(eventClass);
-				event.initEvent(eventName, bubbles, true); // All events created as bubbling and cancelable.
-				event.synthetic = true; // allow detection of synthetic events
-				// The second parameter says go ahead with the default action
-				node.dispatchEvent(event, true);
-			} else  if (node.fireEvent) {
-				// IE-old school style
-				var event = doc.createEventObject();
-				event.synthetic = true; // allow detection of synthetic events
-				node.fireEvent("on" + eventName, event);
-			}
+			var ev = new CustomEvent(type, options.args);
+			el.dispatchEvent(ev);
 		};
 	})();
 
