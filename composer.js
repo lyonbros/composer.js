03af52a117574a015dcc4c1adc1e8b781784f2b9
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-09-24 20:06:01 -0700

    removing global Class export, conflicts with moo (duh)

--- composer.js
+++ composer.js
@@ -388,8 +388,8 @@
 	function Class(obj) { return Base.extend(obj); };
 	Class.extend = Class;
 
-	if(global.Composer) global.Composer.exp0rt({ Class: Class });
-	global.Class = Class;
+	Composer.exp0rt({ Class: Class });
+
 }).apply((typeof exports != 'undefined') ? exports : this);
 
 /**
