0eb408a50085c49398b1d25171d03f0337b8b94c
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-08-30 14:31:05 -0700

    performance tuning Event.trigger. made it about 2.5x as fast by getting rid of some needless copying and using for loops instead of forEach.

--- test/playground.js
+++ test/playground.js
@@ -1,25 +1,73 @@
-var Dog = Composer.RelationalModel.extend({
-	relations: {
-		toys: {
-			collection: Composer.Collection
-		}
-	}
-});
-var Person = Composer.RelationalModel.extend({
-	relations: {
-		pet: {
-			model: Dog
-		}
+var Dog = Composer.Model.extend({ });
+var TestController = Composer.Controller.extend({
+	inject: '#app',
+
+	elements: {
+		'span': 'el_status'
+	},
+	events: {
+		'click input[type=button]': 'run_test'
+	},
+
+	init: function()
+	{
+		this.render();
+	},
+
+	render: function()
+	{
+		this.html('<input type="button" name="test" value="run">&nbsp;<span></span>');
+	},
+
+	run_test: function()
+	{
+		this.el_status.set('html', 'running');
+		setTimeout(function() {
+			test();
+			setTimeout(function() {
+				this.el_status.set('html', 'complete');
+			}.bind(this));
+		}.bind(this))
 	}
 });
 
-var person;
+var dog;
 window.addEvent('domready', function() {
-	person = new Person();
-	person.set({name: 'ruth', pet: {name: 'timmy', toys: [{id: 1, name: 'ropey'}, {id: 2, name: 'bally'}]}});
-	console.log('- json: ', JSON.stringify(person.toJSON()));
-	console.log('---');
-	person.set({pet: {toys: [{id: 2, name: 'sticky'}]}}, {upsert: true});
-	console.log('- json: ', JSON.stringify(person.toJSON()));
+	dog = new Dog();
+
+	var barked = 0;
+	var bitten = 0;
+	var sit = 0;
+	dog.bind('bark', function() { barked++; });
+	dog.bind('bite', function() { bitten++; });
+	dog.bind('sit', function() { sit++; });
+
+	new TestController();
 });
+
+var _perf = {first: window.performance.now(), last: window.performance.now()};
+function perf(where, options)
+{
+	options || (options = {});
+	var now = window.performance.now();
+	if(options.reset) _perf.first = _perf.last = now;
+
+	// `where` should always be n chars, w/ space padding
+	for(var i = 0, n = (24 - where.length); i < n; i++) where = ' ' + where;
+	console.log(where + ': ', now - _perf.last, now - _perf.first);
+	_perf.last = now;
+}
+
+function test(n)
+{
+	n || (n = 99999);
+	perf('start', {reset: true});
+	for(var i = 0; i < n; i++)
+	{
+		dog.trigger('bark');
+		dog.trigger('bite');
+		dog.trigger('sit');
+	}
+	perf('end');
+}
 
