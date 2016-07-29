07f20c2f5cbf7e80c45a57200e830751893de0be
Andrew Danger Lyon <orthecreedence@gmail.com>
2016-07-29 15:35:49 -0500

    just kidding, new 1.2.7

--- composer.js
+++ composer.js
@@ -3105,6 +3105,7 @@
 			var route = false;
 			var match = [];
 			var regex = null;
+			var matched_re = null;
 			for(var re in routes)
 			{
 				regex = new RegExp('^' + re.replace(/\//g, '\\\/') + '$');
@@ -3112,12 +3113,13 @@
 				if(match)
 				{
 					route = routes[re];
+					matched_re = re;
 					break;
 				}
 			}
 			if(!route) return false;
 
-			return {route: route, args: match, regex: regex};
+			return {route: route, args: match, regex: regex, key: matched_re};
 		},
 
 		/**
